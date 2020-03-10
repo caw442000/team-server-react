a9af69ac41434f44e3106df01ee7707396542b32
cedric-winbush <cawinbushjr@gmail.com>
2020-03-09 19:10:01 -0500

    server get call

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+
+const server = express();
+
+server.use(express.json());
+
+const PORT = 5000;
+
+let books = [
+  {
+    id: 1, 
+    title: 'The Prequel',
+    year: 1975
+  },
+  {
+    id: 2, 
+    title: 'Birth of a Legend',
+    year: 1982
+
+  },
+  {
+    id: 3, 
+    title: 'The Fall of A Titan',
+    year: 2018
+
+  }
+];
+
+server.get(`/api/books`, (req, res) => {
+  res.status(200).json(books);
+
+})
+
+server.listen(PORT, () => console.log(`\n server is listening on ${PORT} \n`));
